4b1047739b88321a0c47ec85e553760400b9a27f
Jesus Arrioja <Jarrioja2210@gmail.com>
2023-07-27 22:01:35 -0300

    added swagger docs

--- src/config/swagger.config.js
+++ src/config/swagger.config.js
@@ -2,8 +2,12 @@ export const swaggerOptions = {
   definition: {
     openapi: "3.0.1",
     info: {
-      title: "Documentacion",
+      title: "Documentacion Coder",
       description: "Documentacion de la API",
+      contact: {
+        name: "Jesus Arrioja",
+        email: "hello@jesusarrioja.dev",
+      },
     },
   },
   apis: [`./docs/**/*.yaml`],
