2ea725ba3cb36951d8f641aa7cc9de5ced30d552
Jesus Arrioja <Jarrioja2210@gmail.com>
2023-08-13 13:18:30 -0300

    fix create product error

--- src/presentation/middlewares/authtorization.js
+++ src/presentation/middlewares/authtorization.js
@@ -4,23 +4,24 @@ const authorization = (permission) => {
   return async (req, res, next) => {
     const user = req.user;
     const productId = req.params.productId; // Obtener el ID del producto de los parámetros
+    if (productId) {
+      if (permission === 'deleteProduct' || 'updateProduct') {
+        // Verificar si el usuario es administrador
+        if (user.isAdmin) {
+          return next(); // Admin puede borrar cualquier producto
+        }
 
-    if (permission === 'deleteProduct' || 'updateProduct') {
-      // Verificar si el usuario es administrador
-      if (user.isAdmin) {
-        return next(); // Admin puede borrar cualquier producto
+        // Obtener información del producto, incluido el propietario
+        const manager = new ProductManager();
+        const product = await manager.findById(productId); // Reemplaza esto con la función adecuada para obtener el producto
+
+        // Verificar si el usuario premium es propietario del producto
+        if (user.role.name === 'premium' && product.owner === user.email) {
+          return next(); // Usuario premium puede borrar sus propios productos
+        }
+
+        return res.status(401).send({ message: 'Not authorized' });
       }
-
-      // Obtener información del producto, incluido el propietario
-      const manager = new ProductManager();
-      const product = await manager.findById(productId); // Reemplaza esto con la función adecuada para obtener el producto
-
-      // Verificar si el usuario premium es propietario del producto
-      if (user.role.name === 'premium' && product.owner === user.email) {
-        return next(); // Usuario premium puede borrar sus propios productos
-      }
-
-      return res.status(401).send({ message: 'Not authorized' });
     }
 
     // Resto de la lógica para otros permisos
